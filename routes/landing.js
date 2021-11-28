fca52b026db62f68c9489847bc2c625fd0b820c9
JuniorChang <changjiale@gmail.com>
2021-11-28 15:16:55 +0000

    continue from part 2

--- routes/landing.js
+++ routes/landing.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req,res)=>{
-    res.send("Part 2")
+    res.render('landing/index')
 })
 
+router.get('/products', (req,res)=>{
+    res.render('product/index')
+})
 module.exports = router;
